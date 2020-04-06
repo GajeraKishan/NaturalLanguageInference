3919ce36b4e1d40dfbe0ec87a2ec47706f723998
swethathomas <60117804+swethathomas@users.noreply.github.com>
2020-04-06 16:44:58 -0700

    Update roberta_mnli_basline.py

--- roberta_mnli_basline.py
+++ roberta_mnli_basline.py
@@ -23,14 +23,15 @@ with codecs.open(filepath_labels,'r',encoding='utf-8') as fp:
    lines = fp.readlines()
    for line in lines:
        line=line.strip('\n')
-       labels.append(int(line))
+         
+       labels.append(int(line))#0=entailed,1=not-entailed.
 fp.close()
 
 count=0
 for i in range(0,len(sentences)):
    
     tokens = roberta.encode(sentences[i][0],sentences[i][1])
-    pred=(roberta.predict('mnli', tokens).argmax()).item()
+    pred=(roberta.predict('mnli', tokens).argmax()).item() #entailment=2,#neutral=1,#contradiction=0
     
     if (pred==2 and labels[i]==0) or ((pred==0 or pred==1) and labels[i]==1):
         count+=1
